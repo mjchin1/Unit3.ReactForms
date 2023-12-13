82f53605f6d196a026cc4c9210bbe49b6c9c4c94
Melissa J. Chin <Melissa@Melissas-MacBook-Air.local>
2023-12-13 13:54:06 -0600

    completed sign-up component portion

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+
+export default function SignUpForm() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword]= useState("")
+    const [error, setError] = useState(null)
+    async function handleSubmit(event) {
+        event.preventDefault(); 
+        try{ 
+            const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', { 
+                method: "POST",
+                body: JSON.stringify({username, password})
+            });
+            const result = await response.json();
+            console.log(result);
+
+
+        } catch(error){
+            setError(error.message);
+        }
+        
+
+    }
+
+    return (
+        <>
+            <h2>Sign Up!</h2>
+            {error && <p>{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:<input value={username} onChange={(event) => setUsername(event.target.value)}/>
+                </label>
+                <label>
+                    Password:<input value={password} onChange={(event) => setPassword(event.target.value)}/>
+                </label>
+                <button>Submit</button>
+            </form>
+        </>
+
+    )
+
+}
